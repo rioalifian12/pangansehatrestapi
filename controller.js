f4e1ab3f20ffb5a5d51c812ca643f2a50dc7cbb0
rioalifian12 <rioalifian12@gmail.com>
2023-10-10 17:02:23 +0700

    menampilkan semua data user

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require("./res");
 var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("Aplikasi REST API berjalan!");
+  response.ok("Aplikasi REST API berjalan!", res);
+};
+
+// menampilkan semua data
+exports.getAllUser = function (req, res) {
+  connection.query("SELECT * FROM user", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function (app) {
   var jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+
+  app.route("/user").get(jsonku.getAllUser);
 };
